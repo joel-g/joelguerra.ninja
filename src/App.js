3b6fd6ada8de27a0ce88a50ba7bba25c05fe37fd
Joel Guerra <joelaguerra1@gmail.com>
2017-08-20 18:14:10 -0700

    populates App render

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import './App.css';
 import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentDisplay: 'Home'
+      currentDisplay: 'home'
     }
   }
 
@@ -17,8 +20,18 @@ class App extends Component {
   }
 
   render() {
+    let display;
+    if (this.state.currentDisplay === 'home') {
+      display = <Home clickHandler={e => this.navClickHandler(e)} />
+    } else if (this.state.currentDisplay === 'about me') {
+      display = <About />
+    } else if (this.state.currentDisplay === 'projects') {
+      display = <Projects />
+    } else if (this.state.currentDisplay === 'resume') {
+      display = <Resume />
+    };
     return (
-      <Home clickHandler={e => this.navClickHandler(e)}/>
+      {display}
     );
   }
 }
