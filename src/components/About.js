91b85f309bdd78f2942ee4fbeb0eec5195cc8794
joel-g <joelaguerra1@gmail.com>
2017-08-23 22:02:02 -0700

    defines Back component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Back from './Back';
 import '../App.css';
 
 function About(){
   return(
     <article>
+      <Back />
       About
     </article>
   )
---
+++ src/components/Back.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Back(){
+  return(
+    <button className='back home'>
+      {"<-- BACK"}
+    </ button>
+  )
+}
+
+export default Back;
