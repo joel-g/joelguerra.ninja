39bfe155a15a0f886616d1493db4c7aff65e96ed
joel-g <joelaguerra1@gmail.com>
2017-08-25 17:11:06 -0700

    working on pdf

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import spdf from "simple-react-pdf";
 import Back from './Back';
 import '../App.css';
 
@@ -6,8 +7,11 @@ class Resume extends Component {
   render() {
     return(
       <article>
-      <Back clickHandler={e => this.props.clickHandler(e)}/>
-      Resume
+        <Back clickHandler={e => this.props.clickHandler(e)}/>
+        Resume
+        <spdf.SimplePDF
+          file="./JoelGuerra _Resume5.pdf"
+          />
       </article>
     )
   }
