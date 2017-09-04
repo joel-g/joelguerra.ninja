3b95bf4fd8eeede0d483bb07f2316ccc5a744c2d
Joel Guerra <joelaguerra1@gmail.com>
2017-09-04 12:21:24 -0700

    adds tool logos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import Back from './Back';
-import StartMenu from './StartMenu'
+import $ from 'jquery';
 import '../App.css';
+import Back from './Back';
+import Header from './Header';
+import StartMenu from './StartMenu'
 
-class Projects extends Component{
+class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentProject: 'none'
+    }
+  }
+
+  projectClickHandler(e){
+    this.setState({currentProject: $(e.target).text()})
+  }
+
   render() {
     return(
       <article className='projects-page'>
       <Back clickHandler={e => this.props.clickHandler(e)}/>
+      <Header />
 
       </article>
     )
